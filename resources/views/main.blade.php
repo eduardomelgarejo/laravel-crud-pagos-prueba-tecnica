f91703b17aafa80706aec012d1d67f399daa324d
Eduardo Melgarejo <eduardo.melgarejo@virginiogomez.cl>
2026-03-11 20:02:04 -0300

    fix: corregir url del logo en la navegacion

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,6 +1,6 @@
 <div class="card bg-light">
     <div class="text-center p-2" style="margin-bottom: -20px">
-        <img src="https://hitch.cl/assets/images/hitch_web/hitch_logo.png" width="150px" alt="logo">
+        <img src="https://hitch.cl/assets/img/logo/hitch_logo.webp" width="150px" alt="logo">
     </div>
     <ul class="navbar-nav p-4">
         <li class="nav-item text-nowrap">
